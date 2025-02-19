083de8ab307b8f18ffdc8ecb7306883157dbed29
Daniv2 <125231943+Daniv2@users.noreply.github.com>
2025-02-18 23:54:17 -0700

    Update Layout.tsx

--- project/src/components/Layout.tsx
+++ project/src/components/Layout.tsx
@@ -38,4 +38,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       {/* Sidebar */}
       <aside
         className={`${
-
+          sidebarOpen ? 'translate-x-0' : '-translate-x-full'
+        } fixed inset-y-0 left-0 z-50 w-64 bg-gray-900 text-white transition-transform duration-300 ease-in-out lg:relative lg:translate-x-0`}
+      >
