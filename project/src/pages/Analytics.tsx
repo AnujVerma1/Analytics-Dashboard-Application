7673a788a344266e99b6a32707fab330c0c61968
Daniv2 <125231943+Daniv2@users.noreply.github.com>
2025-02-19 23:01:32 -0700

    Update Analytics.tsx

--- project/src/pages/Analytics.tsx
+++ project/src/pages/Analytics.tsx
@@ -57,6 +57,15 @@ interface DailyStat {
       } finally {
         setLoading(false);
       }
+    };
+
+    if (loading) {
+      return (
+        <div className="flex justify-center">
+          <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+        </div>
+      );
+    }
 );
 };
 
