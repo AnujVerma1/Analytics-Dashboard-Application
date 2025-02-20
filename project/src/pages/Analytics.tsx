9f98894b74e5bb8393a50d07f1382ca97f3ca0d3
Daniv2 <125231943+Daniv2@users.noreply.github.com>
2025-02-19 23:01:38 -0700

    Update Analytics.tsx

--- project/src/pages/Analytics.tsx
+++ project/src/pages/Analytics.tsx
@@ -66,6 +66,15 @@ interface DailyStat {
         </div>
       );
     }
+
+    
+  if (error) {
+    return (
+      <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
+        {error}
+      </div>
+    );
+  }
 );
 };
 
