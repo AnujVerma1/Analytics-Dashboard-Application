da118edc1e40360b82de6c770fc0303fab35cffb
Daniv2 <125231943+Daniv2@users.noreply.github.com>
2025-02-21 12:27:13 -0700

    Update Customers.tsx

---
+++ project/src/pages/Customers.tsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import { supabase } from '../lib/supabase';
+import { Mail, Phone, MapPin } from 'lucide-react';
+
+
+
+
+export default Customers;
