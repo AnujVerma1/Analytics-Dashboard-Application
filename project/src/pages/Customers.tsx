e6bab4ef18d82c39a6ebb33dfad2055638174f85
Daniv2 <125231943+Daniv2@users.noreply.github.com>
2025-02-21 12:31:08 -0700

    Update Customers.tsx

--- project/src/pages/Customers.tsx
+++ project/src/pages/Customers.tsx
@@ -45,5 +45,6 @@ const Customers = () => {
           </div>
  ) : (
   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    {customers.map((customer) => (
 
 export default Customers;
